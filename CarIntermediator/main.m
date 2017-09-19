392110083bbdba2fd2506aa18fc8463fbe5b3afb
李加建 <779454060@qq.com>
2017-09-19 18:51:22 +0800

    Initial Commit

---
+++ CarIntermediator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarIntermediator
+//
+//  Created by 李加建 on 2017/9/19.
+//  Copyright © 2017年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
